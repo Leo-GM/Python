2440cb36a8ea2cdc98a77f248ecaa6819ea94339
Leonardo Guimarães <leo_steam365@hotmail.com>
2022-09-12 20:28:31 -0300

    lab4

--- lab03.py
+++ lab03.py
@@ -1,15 +1,10 @@
-###################################################
-# MC102 - Algoritmos e Programação de Computadores
-# Laboratório 3 - Ingresso do Cinema
-# Nome:Leonardo Henrique Guimarães
-# RA:247225
-###################################################
-
 dia = int(input())
 hora = int(input())
 minuto = int(input())
 estudante = input()
 pagamento = input()
+
+tempo = (hora * 60) + 30
 
 if dia == 1:
    
@@ -18,238 +13,83 @@ if dia == 1:
     if estudante == "N":
         if pagamento == "D":
             valor = valor
-        else:
+        else:   
             valor = valor * (7/10)
     
-    elif estudante == "S":
+    else:
         valor = valor * (1/2)
 
-## HORÁRIO 18:30 +++###
-if (hora >= 18) and (minuto >= 30):
-    if dia == 2:
-        valor = 20
+if tempo <= 1110:
+        
+    if (dia == 2) or (dia == 3) or (dia == 4):
+        valor = 15
         if estudante == "N":
             if pagamento == "D":
                 valor = valor
             else:
                 valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 3:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 4:
-        valor = 30
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 5:
-        valor = 30
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 6:
-        valor = 40
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (7/10)
-        else:
-            valor = valor * (1/2)
-    elif dia == 7:
-        valor = 40
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (2/10)
         else:
             valor = valor * (1/2)
 
-## HORÁRIO 18:30++++##
+    elif (dia == 5) or (dia == 6):
+        valor = 20
+        if estudante == "N":
+            if pagamento == "D":
+                valor = valor
+            else:
+                valor = valor * (1/2)
+        else:
+            valor = valor * (1/2)
 
-elif (hora > 18):
-    if dia == 2:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 3:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 4:
-        valor = 30
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 5:
-        valor = 30
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 6:
-        valor = 40
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (7/10)
-        else:
-            valor = valor * (1/2)
-    elif dia == 7:
-        valor = 40
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (2/10)
-        else:
-            valor = valor * (1/2)
-##HORÁRIO 18:30 ---##
-elif (hora >= 18) and (minuto < 30):
-    if dia == 2:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 3:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 4:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 5:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 6:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
     elif dia == 7:
         valor = 30
         if estudante == "N":
             if pagamento == "D":
                 valor = valor
             else:
-                valor = valor * (2/10)
+                valor = valor * (8/10)
         else:
             valor = valor * (1/2)
-##HORÁRIO 18:30 ---##
-elif (hora < 18):
-    if dia == 2:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 3:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 4:
-        valor = 15
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 5:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 6:
-        valor = 20
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (1/2)
-        else:
-            valor = valor * (1/2)
-    elif dia == 7:
-        valor = 30
-        if estudante == "N":
-            if pagamento == "D":
-                valor = valor
-            else:
-                valor = valor * (2/10)
-        else:
-            valor = valor * (1/2)
+
+if tempo > 1110:
     
+    if (dia == 2) or (dia == 3):
+        valor = 20
+        if estudante == "N":
+            if pagamento == "D":
+                valor = valor
+            else:
+                valor = valor * (1/2)
+        else:
+            valor = valor * (1/2)
+
+    elif (dia == 4) or (dia == 5):
+        valor = 30
+        if estudante == "N":
+            if pagamento == "D":
+                valor = valor
+            else:
+                valor = valor * (1/2)
+        else:
+            valor = valor * (1/2)
+    elif dia == 6:
+        valor = 40
+        if estudante == "N":
+            if pagamento == "D":
+                valor = valor
+            else:
+                valor = valor * (7/10)
+        else:
+            valor = valor * (1/2)
+
+    elif dia == 7:
+        valor = 40
+        if estudante == "N":
+            if pagamento == "D":
+                valor = valor
+            else:
+                valor = valor * (8/10)
+        else:
+            valor = valor * (1/2)
 
 print('Valor do ingresso: R$', format(valor, '.2f').replace('.', ','))
---
+++ lab04.py
@@ -0,0 +1,35 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 4 - Controle de Estoque
+# Nome:Leonardo Henrique Guimarães 
+# RA:247225
+###################################################
+estoque = 0
+n_vendas = 0
+lista =[]
+
+while True:
+    pedidos = int(input())
+    if pedidos == 0:
+        break
+    else:
+        lista.append(pedidos)
+
+for pedido in lista:
+    
+    if pedido < 0:
+
+        if (pedido*(-1)) > estoque:
+            print("Quantidade indisponível para a venda de",pedido * (-1) ,"unidades.")
+        
+        else:
+            estoque = estoque + pedido
+            n_vendas = n_vendas + 1
+    
+    if pedido > 0:
+
+        estoque = estoque + pedido
+
+print("Quantidade de vendas realizadas:", n_vendas)
+
+print("Quantidade em estoque:", estoque)
